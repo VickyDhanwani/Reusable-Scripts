3316436405222605c63e2c47214807da8cfdade7
VickyDhanwani <47933863+VickyDhanwani@users.noreply.github.com>
2020-08-04 13:35:50 +0530

    Update DatabaseProblems.sql

--- DatabaseProblems.sql
+++ DatabaseProblems.sql
@@ -1,1 +1,6 @@
+/*The Employee table holds all employees. Every employee has an Id, a salary, and there is also a column for the department Id.*/
+/* The Department table holds all departments of the company.*/
+/* Write a SQL query to find employees who have the highest salary in each of the departments.*/
 
+select C.Name as 'Department', A.Name as 'Employee', A.Salary from Employee as A, Department as C
+where A.Salary >= all(select B.Salary from Employee as B where B.DepartmentId = A.DepartmentId ) and A.DepartmentId = C.Id;
