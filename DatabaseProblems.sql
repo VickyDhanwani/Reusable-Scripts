beb064eac29b0c5e1f6d45a46654ac29de8e97fb
VickyDhanwani <47933863+VickyDhanwani@users.noreply.github.com>
2020-08-06 17:26:24 +0530

    Update DatabaseProblems.sql

--- DatabaseProblems.sql
+++ DatabaseProblems.sql
@@ -4,3 +4,9 @@
 
 select C.Name as 'Department', A.Name as 'Employee', A.Salary from Employee as A, Department as C
 where A.Salary >= all(select B.Salary from Employee as B where B.DepartmentId = A.DepartmentId ) and A.DepartmentId = C.Id;
+
+
+/*Suppose that a website contains two tables, the Customers table and the Orders table. Write a SQL query to find all customers who never order anything.*/
+
+select Name as Customers from Customers as C
+where C.Id not in (select O.CustomerId from Orders as O);
