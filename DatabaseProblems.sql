39753f48a16015c4f146e88db4b29acc38c6c629
VickyDhanwani <47933863+VickyDhanwani@users.noreply.github.com>
2020-09-06 12:01:28 +0530

    Update DatabaseProblems.sql

--- DatabaseProblems.sql
+++ DatabaseProblems.sql
@@ -38,3 +38,23 @@ where C.Salary = (
     from Employee as A
     where A.Salary < (select Max(B.Salary) from Employee as B)
 );
+
+/*
+Write a SQL query to get the nth highest salary from the Employee table.
+For example, given the above Employee table, the nth highest salary where n = 2 is 200. 
+If there is no nth highest salary, then the query should return null.
+*/
+
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+  DECLARE k INT;
+  SET k = N - 1;
+  RETURN (
+      
+      # Write your MySQL query statement below.
+      SELECT DISTINCT Salary
+      FROM Employee
+      ORDER BY SALARY DESC
+      LIMIT 1 OFFSET k
+  );
+END
